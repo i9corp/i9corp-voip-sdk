e3c4d046f16c01c5255f99bd54b3cd0ee6e523e2
Sileno O. Brito <silenobrito@gmail.com>
2019-11-01 17:54:11 -0300

    Pacote debian

--- extras/deploy-linux.sh
+++ extras/deploy-linux.sh
@@ -25,5 +25,47 @@ ${COMMAND_CMAKE} -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_SH=CMAKE
 ${COMMAND_MAKE}
 ${COMMAND_MAKE} install
 
-${COMMAND_SUDO} ${COMMAND_COPY} -rf ${PROJECT_DIR}/dist/* /usr/
+
+STAGE_DIR=/tmp/i9corp-voip-debian
+SCRIPT=$(readlink -f "$0")
+SCRIPT_PATH=$(dirname "$SCRIPT")
+DIST_DIR=${PROJECT_DIR}/dist
+BUILD_DIR=${STAGE_DIR}/build
+BASE_DIR=${STAGE_DIR}/usr
+ARCH="$(/usr/bin/dpkg --print-architecture)"
+set -e
+
+echo "Starting..."
+echo "Arch: ${ARCH}"
+echo "Directory (dist): ${DIST_DIR}"
+echo "Directory (base): ${BASE_DIR}"
+echo "Directory (build): ${BUILD_DIR}"
+echo "Directory (stage): ${STAGE_DIR}"
+
+${COMMAND_SUDO} ${COMMAND_RM} -rf ${STAGE_DIR}
+mkdir -p ${STAGE_DIR}/DEBIAN
+mkdir -p ${BASE_DIR}
+
+echo "Creating package structure..."
+
+cat >> ${STAGE_DIR}/DEBIAN/control <<EOF
+Package: i9corp-voip
+Priority: extra
+Section: net
+Maintainer: I9CORP <repo@i9corp.com.br>
+Version: @VERSION@
+Depends: libpjproject-dev
+Description: I9Corp Biblioteca para Voip
+EOF
+
+echo "Architecture: ${ARCH}" >> ${STAGE_DIR}/DEBIAN/control
+
+
+${COMMAND_SUDO} ${COMMAND_COPY} -rf ${PROJECT_DIR}/dist/* ${BASE_DIR}/
+
+chmod 755 ${STAGE_DIR}/DEBIAN/control
+
+echo "Creating package..."
+dpkg -b ${STAGE_DIR} ${DIST_DIR}/i9corp-voip_@VERSION@_${ARCH}.deb
+echo "Done!";
 
